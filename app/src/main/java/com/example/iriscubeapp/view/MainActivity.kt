176136f703b1acc9e892519c40c679f2a806ccff
LPT-STORRICELLI\Ribes <simone.torricelli.00@gmail.com>
2021-09-27 14:02:01 +0200

    Init Branch MVP

--- app/src/main/java/com/example/iriscubeapp/view/MainActivity.kt
+++ app/src/main/java/com/example/iriscubeapp/view/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity(),ContractInterface.View{
      * [AppCompatActivity] functions
      * */
 
+    //MVP Branch
+
     private var presenter: Presenter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
